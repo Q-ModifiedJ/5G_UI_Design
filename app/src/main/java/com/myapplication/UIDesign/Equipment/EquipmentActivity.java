bbd970f2147795e8952165b3f42d5e4b239196fc
QModifedJ <1638631691@qq.com>
2020-08-16 18:44:34 +0800

    new

--- app/src/main/java/com/myapplication/UIDesign/Equipment/EquipmentActivity.java
+++ app/src/main/java/com/myapplication/UIDesign/Equipment/EquipmentActivity.java
@@ -1,54 +1,61 @@
 package com.myapplication.UIDesign.Equipment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.myapplication.UIDesign.BaseStation.BaseStationItem;
+import com.myapplication.UIDesign.BaseStation.BaseStationItemAdapter;
 import com.myapplication.UIDesign.R;
 
-/**
- * 类名：EquipmentActivity
- * 功能：用于处理"接入设备"的Activity,主要实现接入设备信息的展示
- * 备注：此版本只能实现三个设备的展示
- */
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class EquipmentActivity extends AppCompatActivity {
+    private List<EquipmentItem> equipmentItems=new ArrayList<>();
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipment);
-
-        Equipment equip1, equip2, equip3;
-        Button equip1_button, equip2_button, equip3_button;
-        TextView equip1_text, equip2_text, equip3_text;
-
-        //以下三个Equipment类对应要显示的三个设备，使用构造函数初始化
-        equip1 = new Equipment("ConcertHall-Ring_CSG1", "Failure", "Normal", "2020-04-12 20:55:21");
-        equip2 = new Equipment("ConcertHall-Ring_CSG3", "Online", "Warning", "2020-04-12 20:55:21");
-        equip3 = new Equipment("ConcertHall-Ring_CSG4", "Planning", "Unknown", "2020-04-12 20:55:21");
-
-        //对于每一个设备，用一个Button显示名称(以实现点击功能)，用一个TextView显示其他信息
-        equip1_text = (TextView) findViewById(R.id.equipment_text1);
-        equip2_text = (TextView) findViewById(R.id.equipment_text2);
-        equip3_text = (TextView) findViewById(R.id.equipment_text3);
-        equip1_button = (Button) findViewById(R.id.equipment_button1);
-        equip2_button = (Button) findViewById(R.id.equipment_button2);
-        equip3_button = (Button) findViewById(R.id.equipment_button3);
-
-        //设置每个组件的文本,其余设置见布局文件
-        equip1_button.setText(equip1.getName());
-        equip1_text.setText(equip1.toString());
-
-        equip2_button.setText(equip2.getName());
-        equip2_text.setText(equip2.toString());
-
-        equip3_button.setText(equip3.getName());
-        equip3_text.setText(equip3.toString());
-
         this.setTitle(getClass().getSimpleName());
+
+        InitEquipmentItems();
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.equipment_recycler_view);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        EquipmentItemAdapter equipmentItemAdapter=new EquipmentItemAdapter(equipmentItems);
+        recyclerView.setAdapter(equipmentItemAdapter);
+    }
+
+    public void InitEquipmentItems(){
+        EquipmentItem equipmentItem1=new EquipmentItem("CentralPark-Ring1-gNodeB1",
+                "上线状态   Failure","运行状态   Normal",
+                "上线时间   2020-04-12 20:55:21");
+        equipmentItems.add(equipmentItem1);
+        EquipmentItem equipmentItem2=new EquipmentItem("CentralPark-Ring1-gNodeB2",
+                "上线状态   Online","运行状态   Normal",
+                "上线时间   2020-04-12 20:55:21");
+        equipmentItems.add(equipmentItem2);
+        EquipmentItem equipmentItem3=new EquipmentItem("CentralPark-Ring1-gNodeB2",
+                "上线状态   Online","运行状态   Normal",
+                "上线时间   2020-04-12 20:55:21");
+        equipmentItems.add(equipmentItem3);
+        EquipmentItem equipmentItem4=new EquipmentItem("CentralPark-Ring1-gNodeB2",
+                "上线状态   Online","运行状态   Normal",
+                "上线时间   2020-04-12 20:55:21");
+        equipmentItems.add(equipmentItem4);
+        EquipmentItem equipmentItem5=new EquipmentItem("CentralPark-Ring1-gNodeB2",
+                "上线状态   Online","运行状态   Normal",
+                "上线时间   2020-04-12 20:55:21");
+        equipmentItems.add(equipmentItem5);
+        EquipmentItem equipmentItem6=new EquipmentItem("CentralPark-Ring1-gNodeB2",
+                "上线状态   Online","运行状态   Normal",
+                "上线时间   2020-04-12 20:55:21");
+        equipmentItems.add(equipmentItem6);
     }
 }
